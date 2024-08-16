6c97a6f7eb31af32492acbb57995ebb7638e3af3
Freemasonry <95870255+MasonGroup@users.noreply.github.com>
2024-08-16 04:02:19 +0300

    Add files via upload

---
+++ PEEKABOO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Windows.Forms;
+
+class Program
+{
+    static void Main()
+    {
+        MessageBox.Show("Gangsta freemasonry made me do it", "PEEKABOO");
+        Environment.Exit(0);
+    }
+}
